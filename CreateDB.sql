1863fdb0ab515591387984d7024686fca7ee38d4
solbon <nayrero@mail.ru>
2021-03-02 16:35:26 +0800

    include createdb.sql

---
+++ CreateDB.sql
@@ -0,0 +1,12 @@
+-- Database: testdb
+
+-- DROP DATABASE testdb;
+
+CREATE DATABASE testdb
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Russian_Russia.1251'
+    LC_CTYPE = 'Russian_Russia.1251'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
